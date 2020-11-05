681d93df534e43e2a7523727657778142bc55905
Jozen Martinez <hi@jozen.blue>
2020-11-05 18:01:47 +0800

    Add markdown export

--- components/ChangeItem.jsx
+++ components/ChangeItem.jsx
@@ -1,15 +1,44 @@
 import { useState } from 'react';
 import dayjs from 'dayjs';
+import { GoMarkdown } from 'react-icons/go';
 import ChangeList from './ChangeList';
 
 export default function ChangeItem({ change, defaultCollapsed }) {
   const date = dayjs(change.time);
   const [collapsed, setCollapsed] = useState(defaultCollapsed);
+  const [customFormatting, setCustomFormatting] = useState(null);
 
   const handleClick = () => {
     // eslint-disable-next-line no-restricted-globals
     history.replaceState(null, null, `#${change.time}`);
     setCollapsed(false);
+  };
+
+  const discordMd = () => {
+    const dateStr = date.format('DD/MM/YYYY');
+    const text = [`**${dateStr}**`];
+    text.push('');
+    change.changes.forEach((list) => {
+      if (list.name) text.push(`**${list.name}**`);
+      if (list.summary) text.push(list.summary);
+      if (list.new) list.new.forEach((item) => text.push(`- ${item}`));
+      if (list.update) list.update.forEach((item) => text.push(`- ${item}`));
+      if (list.fix) list.fix.forEach((item) => text.push(`- ${item}`));
+      if (list.remove) list.remove.forEach((item) => text.push(`- ${item}`));
+      text.push('');
+    });
+    text.push('@everyone');
+
+    /*
+      Messy transformation for Discord styled markdown.
+      Turn into quote block, trim triple newlines
+    */
+    const discordFormatted = text.join('\n')
+      .replace(/[\r\n]{3}/g, '\n\n')
+      .split('\n')
+      .map((x) => `> ${x}`)
+      .join('\n');
+    setCustomFormatting(discordFormatted);
   };
 
   return (
@@ -21,12 +50,22 @@ export default function ChangeItem({ change, defaultCollapsed }) {
       onClick={handleClick}
       onKeyDown={handleClick}
     >
-      <div className="date">
+      <div className="panel-left">
         <div className="day">{date.format('D')}</div>
         <div className="month">{date.format('MMM')}</div>
         <div className="year">{date.format('YYYY')}</div>
+        <div className="buttons">
+          <button
+            title="Get Discord Styled Markdown"
+            type="button"
+            tabIndex={-1}
+            onClick={discordMd}
+          >
+            <GoMarkdown />
+          </button>
+        </div>
       </div>
-      <ChangeList changes={change.changes} />
+      <ChangeList changes={change.changes} customFormatting={customFormatting} />
     </section>
   );
 }
--- components/ChangeList.jsx
+++ components/ChangeList.jsx
@@ -3,22 +3,31 @@ import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import ChangeListItem from './ChangeListItem';
 
-export default function ChangeList({ changes }) {
+export default function ChangeList({ changes, customFormatting = null }) {
   return (
     <>
       <div className="list">
-        {changes.map((list, i) => (
-          <Fragment key={i}>
-            {list.name && <h2>{list.name}</h2>}
-            {list.summary && <ReactMarkdown plugins={[gfm]}>{list.summary}</ReactMarkdown>}
-            <ul className="change-item">
-              {list.new && list.new.map((item) => <ChangeListItem key={item} content={item} type="new" />)}
-              {list.update && list.update.map((item) => <ChangeListItem key={item} content={item} type="update" />)}
-              {list.fix && list.fix.map((item) => <ChangeListItem key={item} content={item} type="fix" />)}
-              {list.remove && list.remove.map((item) => <ChangeListItem key={item} content={item} type="remove" />)}
-            </ul>
-          </Fragment>
-        ))}
+        {customFormatting
+          ? (
+            <textarea
+              value={customFormatting}
+              onFocus={(evt) => evt.target.select()}
+              onClick={(evt) => evt.target.select()}
+              readOnly
+            />
+          )
+          : changes.map((list, i) => (
+            <Fragment key={i}>
+              {list.name && <h2>{list.name}</h2>}
+              {list.summary && <ReactMarkdown plugins={[gfm]}>{list.summary}</ReactMarkdown>}
+              <ul className="change-item">
+                {list.new && list.new.map((item) => <ChangeListItem key={item} content={item} type="new" />)}
+                {list.update && list.update.map((item) => <ChangeListItem key={item} content={item} type="update" />)}
+                {list.fix && list.fix.map((item) => <ChangeListItem key={item} content={item} type="fix" />)}
+                {list.remove && list.remove.map((item) => <ChangeListItem key={item} content={item} type="remove" />)}
+              </ul>
+            </Fragment>
+          ))}
       </div>
     </>
   );
